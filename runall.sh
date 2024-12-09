af644d5e3213a3566bbec4757efb71314e1e38c9
Paramveer Singh <paramveer.singh238@outlook.com>
2024-12-09 09:54:08 -0800

    Add and test wordcount scripts

---
+++ runall.sh
@@ -0,0 +1,12 @@
+python scripts/wordcount.py \
+    --input_file=data/isles.txt \
+    --output_file=results/isles.dat
+python scripts/wordcount.py \
+    --input_file=data/abyss.txt \
+    --output_file=results/abyss.dat
+python scripts/wordcount.py \
+    --input_file=data/last.txt \
+    --output_file=results/last.dat
+python scripts/wordcount.py \
+    --input_file=data/sierra.txt \
+    --output_file=results/sierra.dat
